01dd14a62e44d3cee7ecde94048b3e4a1831b059
Seungeon Kim <skim1260@terpmail.umd.edu>
2022-10-30 22:26:19 -0400

    added INST326.py

--- INST326.py
+++ INST326.py
@@ -1,17 +1,6 @@
-""" A simple social network for tracking connections betweem people. """
+def hello():
+    print("hello")
 
-class person:
-    """ A person in the social network.
 
-    Attributes:
-        name(str): the person's name
-        connections (set of Person): other people in the social network.
-            who know this person.  
-    """
-
-    def hello():
-        print("hello")
-        
-
-    def goodbye():
-        print("goodbye")
+def goodbye():
+    print("goodbye")
